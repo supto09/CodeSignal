78abb866b722872f65cc9fcb8f0d97691ec3409a
supto09 <supto09apee@gmail.com>
2020-09-04 22:14:36 +0600

    Few more solution

--- src/main/kotlin/interview/sumInRange.kt
+++ src/main/kotlin/interview/sumInRange.kt
@@ -1,22 +1,21 @@
 package interview
 
 fun sumInRange(nums: MutableList<Int>, queries: MutableList<MutableList<Int>>): Int {
-    val numberPositionMap = mutableMapOf<Int,Int>()
-    for(index in nums.indices){
-        val number = nums[index]
-        numberPositionMap[index] = number
+    if (nums.size == 0 || queries.size == 0) return 0
+    val mod = 1000000007
+    val map = HashMap<Int, Int>()
+    var sumPre = nums[0]
+    map[0] = sumPre
+    for (i in 1 until nums.size) {
+        sumPre += nums[i]
+        map[i] = sumPre % mod
     }
-
-
     var sum = 0
-    for(query in queries){
-        val firstIndex = query[0]
-        val secondIndex = query[1]
-
-        for(index in firstIndex..secondIndex){
-            val value =  numberPositionMap[index]?:0
-            sum = sum + value
-        }
+    for (i in queries.indices) {
+        if (queries[i][0] == 0)
+            sum = sum % mod + map[queries[i][1]]!! % mod
+        else
+            sum = sum % mod + map[queries[i][1]]!! - map[queries[i][0] - 1]!! % mod
     }
-    return sum
+    return (sum + mod) % mod
 }
